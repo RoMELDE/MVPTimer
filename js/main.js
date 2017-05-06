6a96d519ce8ee630a741c452f67dba711791e72b
WindWT <leibao0723@gmail.com>
2017-05-06 22:47:46 +0800

    add fast switch

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 $(function () {
-    var init = function () {
-        if (localStorage["lastSelected"]) {
-            $('#TimerTag').val(localStorage["lastSelected"]);
-        }
-    }();
-
-    if (Notification && Notification.permission !== "granted") {
-        Notification.requestPermission(function (status) {
-            if (Notification.permission !== status) {
-                Notification.permission = status;
-            }
-        });
-    }
 
     var writeLog = function (text, tag, id) {
         text = "【" + tag + padLeft(id) + "】" + text + "\t" + new Date().toISOString();
@@ -50,30 +37,93 @@ $(function () {
         };
     }
 
-    $('#addTimer30').click(function () {
-        var tag = $('#TimerTag').val();
-        createWorker(30, tag);
-    })
-    $('#addTimer120').click(function () {
-        var tag = $('#TimerTag').val();
-        createWorker(120, tag);
-    })
-    $('#addTimerCustom').click(function () {
-        var tag = $('#TimerTag').val();
-        createWorker(parseInt($('#addTimerCustomMinute').val() || 1), tag);
-    })
-
-    setInterval(function () {
-        if (!Notification) {
-            alert("浏览器不支持HTML5通知！");
-        }
-        else {
-            if (Notification.permission !== "granted") {
-                $('#NotificationDeniedText').show();
+    var initControl = function (data) {
+        $.each(data, function (i, o) {
+            var $div = $('<div>').addClass("monster");
+            var $mark = $('<div>').addClass("monster-mark");
+            if (o.Type == "MINI") {
+                $mark.addClass("monster-mini");
             }
             else {
-                $('#NotificationDeniedText').hide();
+                $mark.addClass("monster-mvp");
             }
+            $div.append($mark);
+            $div.css("background-image", "url('img/Face/" + o.Icon + ".png')");
+            $div.data('data', o);
+            $div.click(function () {
+                var current = $(this).data('data');
+                $('#TimerTag').val(current.NameZh);
+                if (current.Type == "MINI") {
+                    createWorker(30, current.NameZh);
+                }
+                else {
+                    createWorker(120, current.NameZh);
+                }
+            });
+            $('#timerList').append($div);
+        });
+
+        $('#addTimer30').click(function () {
+            var tag = $('#TimerTag').val();
+            createWorker(30, tag);
+        })
+        $('#addTimer120').click(function () {
+            var tag = $('#TimerTag').val();
+            createWorker(120, tag);
+        })
+        $('#addTimerCustom').click(function () {
+            var tag = $('#TimerTag').val();
+            createWorker(parseInt($('#addTimerCustomMinute').val() || 1), tag);
+        })
+    };
+
+    var init = function () {
+        //init Notification
+        if (Notification && Notification.permission !== "granted") {
+            Notification.requestPermission(function (status) {
+                if (Notification.permission !== status) {
+                    Notification.permission = status;
+                }
+            });
         }
-    }, 500);
+        setInterval(function () {
+            if (!Notification) {
+                alert("浏览器不支持HTML5通知！");
+            }
+            else {
+                if (Notification.permission !== "granted") {
+                    $('#NotificationDeniedText').show();
+                }
+                else {
+                    $('#NotificationDeniedText').hide();
+                }
+            }
+        }, 500);
+
+        if (localStorage["lastSelected"]) {
+            $('#TimerTag').val(localStorage["lastSelected"]);
+        }
+
+        var getMonster = function () {
+            $.get("data/Table_Monster.json").done(function (d) {
+                localStorage["monster"] = JSON.stringify(d);
+                localStorage["lastUpdate"] = JSON.stringify(new Date());
+                initControl(d);
+            });
+        }
+        if (!localStorage["monster"]) {
+            getMonster();
+        }
+        else {
+            $.get("data/lastUpdate.json").done(function (d) {
+                if (localStorage["lastUpdate"] && JSON.parse(localStorage["lastUpdate"]) > d) {
+                    initControl(JSON.parse(localStorage["monster"]));
+                }
+                else {
+                    getMonster();
+                }
+            });
+        }
+
+    }();
 });
